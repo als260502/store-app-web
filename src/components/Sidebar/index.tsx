4689578fc1883cc48cf1e69490d7dda8dfdf9189
André Souza <als260602@msn.com>
2022-08-22 09:17:13 -0300

    FIX: logo size

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,7 +15,7 @@ export const Sidebar = ({ children }: Props) => {
       <div className="h-full w-[150px] md:w-[200px] bg-gray-100 py-8 px-2 flex flex-col border-r-2 border-gray-900 border-opacity-25 ">
         <div className="flex flex-col items-center pb-8">
           <div className="w-28 flex items-center justify-center rounded-full overflow-hidden border border-blue-400 shadow-xl">
-            <img src={logo} alt="Logo" width={100} height={100} />
+            <img src={logo} alt="Logo" />
           </div>
           <div className="py-0.5 mt-2 flex flex-row gap-1 items-center">
             <span className="text-sm font-bold text-gray-500">
